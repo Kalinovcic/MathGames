880f90978228eba6e96b2113a685bb38bb48e863
kalinovcic <lovro.kalinovcic@gmail.com>
2017-09-09 21:25:44 +0200

    More

--- src/net/kalinovcic/mathgame/TableShooterStage.java
+++ src/net/kalinovcic/mathgame/TableShooterStage.java
@@ -20,54 +20,203 @@ public class TableShooterStage extends Stage
         float x;
         float y;
         float vy;
+        boolean inTable;
+        boolean hit;
+        boolean red;
+
+        void setX()
+        {
+            while (true)
+            {
+                boolean success = true;
+
+                x = game.random.nextFloat() * (game.width * 0.6f - 160) + 80;
+                
+                int count = Math.min(fallingNumbers.size(), 3);
+                for (int i = fallingNumbers.size() - count; i < fallingNumbers.size(); i++)
+                {
+                    if (Math.abs(x - fallingNumbers.get(i).x) < 100)
+                        success = false;
+                }
+                
+                if (success) break;
+            }
+        }
         
         void update(float dt)
         {
             y += vy * dt;
+            if (y > game.height - 10) y = game.height - 10;
+            if (x > game.width) setX();
         }
         
         void render(Graphics2D g)
         {
             Font smallFont = Art.FONT.deriveFont(game.height * (50.0f / 1080.0f));
-            GraphicsHelper.drawString(g, smallFont, number + "", x, y - 7, 0.5f, Color.WHITE);
+            GraphicsHelper.drawString(g, smallFont, number + "", x, y - 7, 0.5f, red ? Color.RED : Color.WHITE);
+        }
+    }
+    
+    class Bullet
+    {
+        float x;
+        float y;
+        float vy;
+        boolean dead;
+
+        void update(float dt)
+        {
+            y += vy * dt;
+            for (FallingNumber number : fallingNumbers)
+            {
+                if (y > number.y) continue;
+                if (Math.abs(x - number.x) > (int)Math.max(Math.ceil(Math.log10(number.number)), 1) * 25) continue;
+                if (Math.abs(y - number.y) > 30.0f) continue;
+                
+                number.hit = true;
+                dead = true;
+                break;
+            }
+            
+            if (y < 0) dead = true;
+        }
+        
+        void render(Graphics2D g)
+        {
+            g.setColor(Color.WHITE);
+            g.fillOval((int)(x - 10), (int)(y - 10), 20, 20);
         }
     }
     
     public List<FallingNumber> fallingNumbers = new ArrayList<FallingNumber>();
+    public List<FallingNumber> numbersToDrop = new ArrayList<FallingNumber>();
+    public List<Bullet> bullets = new ArrayList<Bullet>();
+
+    private boolean[] table = new boolean[100];
     
     public TableShooterStage(Game game)
     {
         super(game);
         this.isOpaque = true;
+        
+        for (int x = 1; x <= 10; x++)
+            for (int y = 1; y <= 10; y++)
+                if (x == 1 || y == 1 || x == 10 || y == 10)
+                    table[(x - 1) * 10 + (y - 1)] = true;
+        
+        for (int i = 1; i <= 100; i++)
+        {
+            if (table[i - 1]) continue;
+
+            FallingNumber number = new FallingNumber();
+            number.number = i;
+            number.inTable = false;
+            
+            for (int x = 1; x <= 10; x++)
+                for (int y = 1; y <= 10; y++)
+                    if (x * y == number.number)
+                        number.inTable = true;
+            
+            numbersToDrop.add(number);
+        }
+        
+        for (int i = 0; i < 1000; i++)
+        {
+            int x = game.random.nextInt(numbersToDrop.size());
+            int y = game.random.nextInt(numbersToDrop.size());
+            FallingNumber a = numbersToDrop.get(x);
+            FallingNumber b = numbersToDrop.get(y);
+            numbersToDrop.set(x, b);
+            numbersToDrop.set(y, a);
+        }
     }
     
-    private int numberSpawnCounter = 0;
+    private float numberSpawnCounter = 0;
 
     private float boatX = 150.0f;
+    private float boatRotationTime = 0;
+    private boolean boatFacingRight = true;
     
     @Override
     public void update(float dt)
     {
-        int numberSpawnRate = 300;
+        float numberSpawnRate = 2;
         
-        numberSpawnCounter--;
-        if (numberSpawnCounter <= 0)
+        numberSpawnCounter += dt;
+        while (numberSpawnCounter >= numberSpawnRate)
         {
-            FallingNumber number = new FallingNumber();
-            number.number = game.random.nextInt(100) + 1;
-            number.x = game.random.nextFloat() * (game.width * 0.6f - 160) + 80;
-            number.y = 0;
-            number.vy = 50;
+            numberSpawnCounter -= numberSpawnRate;
 
-            fallingNumbers.add(number);
-            numberSpawnCounter = numberSpawnRate;
+            if (numbersToDrop.size() != 0)
+            {
+                int last = numbersToDrop.size() - 1;
+                FallingNumber number = numbersToDrop.get(last);
+                numbersToDrop.remove(last);
+
+                number.setX();
+                number.y = 0;
+                number.vy = 50;
+
+                fallingNumbers.add(number);
+            }
+        }
+
+        for (int i = 0; i < bullets.size(); i++)
+        {
+            Bullet bullet = bullets.get(i);
+            bullet.update(dt);
+            if (bullet.dead)
+            {
+                int last = bullets.size() - 1;
+                bullets.set(i, bullets.get(last));
+                bullets.remove(last);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < fallingNumbers.size(); i++)
+        {
+            FallingNumber number = fallingNumbers.get(i);
+            number.update(dt);
+            if (number.hit)
+            {
+                if (number.inTable)
+                {
+                    number.red = true;
+                    number.hit = false;
+                }
+                else
+                {
+                    int last = fallingNumbers.size() - 1;
+                    fallingNumbers.set(i, fallingNumbers.get(last));
+                    fallingNumbers.remove(last);
+                    i--;
+                }
+            }
+        }
+
+        float move = 0.0f;
+        if (Input.isKeyDown(KeyEvent.VK_LEFT )) move = -1.0f;
+        if (Input.isKeyDown(KeyEvent.VK_RIGHT)) move =  1.0f;
+        if (Input.isKeyPressed(KeyEvent.VK_SPACE))
+        {
+            Bullet bullet = new Bullet();
+            bullet.x = boatX + 100;
+            bullet.y = game.height * 0.7f - 100;
+            bullet.vy = -400.0f;
+            bullets.add(bullet);
         }
         
-        for (FallingNumber number : fallingNumbers)
-            number.update(dt);
-
-        if (Input.isKeyDown(KeyEvent.VK_LEFT )) boatX -= 500.0f * dt;
-        if (Input.isKeyDown(KeyEvent.VK_RIGHT)) boatX += 500.0f * dt;
+        if (move != 0.0f)
+        {
+            boatX += 500.0f * move * dt;
+            boatRotationTime += 8.0 * dt;
+            boatFacingRight = move > 0.0f;
+        }
+        else
+        {
+            boatRotationTime += 4.0 * dt;
+        }
 
         float boatWidth = 200.0f;
         float width = game.width * 0.6f - boatWidth;
@@ -104,6 +253,9 @@ public class TableShooterStage extends Stage
         for (FallingNumber number : fallingNumbers)
             number.render(g);
         
+        for (Bullet bullet : bullets)
+            bullet.render(g);
+        
         
 
         g.setColor(Color.DARK_GRAY);
@@ -118,7 +270,9 @@ public class TableShooterStage extends Stage
         float boatY = waterLY + 10;
         
         AffineTransform boatTransform = AffineTransform.getTranslateInstance(boatX + boatWidth * 0.5, boatY);
-        boatTransform.rotate(Math.sin(game.secondsSinceStart * 4.0) * 0.1);
+        if (boatFacingRight)
+            boatTransform.scale(-1, 1);
+        boatTransform.rotate(Math.sin(boatRotationTime) * 0.1);
         g.setTransform(boatTransform);
         g.drawImage(Art.BOAT, -(int)(boatWidth * 0.5), -(int)(boatHeight), (int)(boatWidth * 0.5), 0, 0, 0, Art.BOAT.getWidth(), Art.BOAT.getHeight(), null);
         g.setTransform(new AffineTransform());
@@ -143,7 +297,10 @@ public class TableShooterStage extends Stage
                     g.drawLine(waterHX + 10, lineY, game.width - 10, lineY);
                 }
                 
-                GraphicsHelper.drawString(g, smallFont, i * j + "", x, y - 7, 0.5f, Color.WHITE);
+                if (table[(j - 1) * 10 + (i - 1)])
+                {
+                    GraphicsHelper.drawString(g, smallFont, i * j + "", x, y - 7, 0.5f, Color.WHITE);
+                }
             }
         }
     }
